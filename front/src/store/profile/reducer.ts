71c51de22aa449eb4de9c2ecd1c61a346603aed1
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-29 23:23:45 +0900

    feat: 住所入力用のreduxを作成

--- front/src/store/profile/reducer.ts
+++ front/src/store/profile/reducer.ts
@@ -3,18 +3,27 @@ import { Profile } from "../../domain/entity/profile";
 import profileActions from './actions'
 
 const init: Profile = {
-  name: '',
-  description: '',
-  birthday: '',
-  gender: ''
+	name: '',
+	description: '',
+	birthday: '',
+	gender: '',
+	address: {
+		postalcode: '',
+		prefecture: '',
+		city: '',
+		restAddress: ''
+	}
 }
 
-const profileReducer = reducerWithInitialState(init).case(
-  profileActions.setProfile,
-  (state, payload) => ({
-    ...state,
-    ...payload
-  })
-)
+const profileReducer = reducerWithInitialState(init)
+	.case(profileActions.setProfile,(state, payload) => ({
+		...state,
+		...payload
+	}))
+
+	.case(profileActions.setAddress, (state, payload) => ({
+		...state,
+		address: { ...state.address, ...payload}
+	}))
 
 export default profileReducer
